563a1b847e1988b78800cf19b8e6430360b919ee
icecreamsoldier <mariana.9000@hotmail.com>
2019-01-28 18:25:29 -0600

    help

---
+++ Lab2/README.txt
@@ -0,0 +1,55 @@
+/**********************************************************************
+ *  README.txt
+ *  TC3022 - Homework 2: Rainbow circle
+ **********************************************************************/
+
+/**********************************************************************
+* What is your name?
+***********************************************************************/
+
+<Replace this with your response--remember to delete the angle brackets>
+
+
+
+/**********************************************************************
+* What browser and operating system did you test your program with?
+***********************************************************************/
+
+<Replace this with your response>
+
+
+
+/**********************************************************************
+* Did you refer to the hint file to complete the assignment, or did
+* you do it entirely from the start file?
+***********************************************************************/
+
+<Replace this with your response!>
+
+
+
+/**********************************************************************
+* Approximately how many hours did you spend working on this assignment?
+***********************************************************************/
+
+<Replace this with your response!>
+
+
+
+/**********************************************************************
+ * Describe any problems you encountered in this assignment.
+ * What was hard, or what should we warn students about in the future?
+ * How can I make this assignment better?
+ **********************************************************************/
+
+<Replace this with your response!>
+
+
+
+/**********************************************************************
+ * List any other comments (about the assignment or your submission)
+ * here. Feel free to provide any feedback on what you learned from
+ * doing the assignment, whether you enjoyed doing it, etc.
+ **********************************************************************/
+
+<Replace this with your response!>
---
+++ Lab2/rainbowCircleStart.js
@@ -0,0 +1,91 @@
+var scene;
+var camera;
+initializeScene();
+renderScene();
+
+function initializeScene() {
+    if (Detector.webgl) {
+        renderer = new THREE.WebGLRenderer({
+            antialias: true
+        });
+    } else {
+        renderer = new THREE.CanvasRenderer();
+    }
+
+    renderer.setClearColor(0x000000, 1);
+
+    canvasWidth = 600;
+    canvasHeight = 600;
+
+    renderer.setSize(canvasWidth, canvasHeight);
+    document.getElementById("canvas").appendChild(renderer.domElement);
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(85, canvasWidth / canvasHeight, 1, 100);
+
+    camera.position.set(0, 0, 20);
+
+    camera.lookAt(scene.position);
+
+    scene.add(camera);
+
+    var rainbowCircleGeometry = new THREE.Geometry();
+
+    //We rotate around the circle incrementally, adding vertices outward to one "spoke" at a time.
+    for (var d = 0; d <= 360; d = d + 10) {
+        var angle = Math.PI * (d / 180);
+        for (var i = 1; i <= 17; i++) {
+            //This is the next vertex out. Because it's the middle vertex on the spoke, it's
+            //part of two different faces.
+            rainbowCircleGeometry.vertices.push(new THREE.Vector3(Math.sin(angle) * i, Math.cos(angle) * i, 0));
+            //Create the face if there's at least 3 vertices
+            if (rainbowCircleGeometry.vertices.length > 17) {
+                var result = rainbowCircleGeometry.vertices.length / 17;
+                if ((Number.isInteger(result)) == true) {
+                    rainbowCircleGeometry.faces.push(new THREE.Face3(
+                        rainbowCircleGeometry.vertices.length - 1
+                        , rainbowCircleGeometry.vertices.length - 2
+                        , rainbowCircleGeometry.vertices.length - 18));
+                    rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[0] = new THREE.Color(Math.cos(angle), Math.sin(angle), 0.5);
+                    rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[1] = new THREE.Color(Math.cos(angle), Math.sin(angle), 0.5);
+                    rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[2] = new THREE.Color(Math.cos(angle), Math.sin(angle), 0.5);
+                } else {
+                    rainbowCircleGeometry.faces.push(new THREE.Face3(
+                        rainbowCircleGeometry.vertices.length - 1
+                        , rainbowCircleGeometry.vertices.length - 17
+                        , rainbowCircleGeometry.vertices.length - 18));
+                    rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[0] = new THREE.Color(Math.cos(angle), Math.sin(angle), 0.5);
+                    rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[1] = new THREE.Color(Math.cos(angle), Math.sin(angle), 0.5);
+                    rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[2] = new THREE.Color(Math.cos(angle), Math.sin(angle), 0.5);
+                    if ((Number.isInteger(result)) == false) {
+                        rainbowCircleGeometry.faces.push(new THREE.Face3(
+                            rainbowCircleGeometry.vertices.length - 1
+                            , rainbowCircleGeometry.vertices.length - 2
+                            , rainbowCircleGeometry.vertices.length - 18));
+                        rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[0] = new THREE.Color(Math.cos(angle), Math.sin(angle), 0.5);
+                        rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[1] = new THREE.Color(Math.cos(angle), Math.sin(angle), 0.5);
+                        rainbowCircleGeometry.faces[rainbowCircleGeometry.faces.length - 1].vertexColors[2] = new THREE.Color(Math.cos(angle), Math.sin(angle), 0.5);
+                    }
+
+
+                }
+            }
+        }
+    }
+
+    var rainbowCircleMat = new THREE.MeshBasicMaterial({
+        vertexColors: THREE.VertexColors,
+        wireframe: true,
+        side: THREE.DoubleSide
+    });
+
+    var rainbowCircleMesh = new THREE.Mesh(rainbowCircleGeometry, rainbowCircleMat);
+
+    scene.add(rainbowCircleMesh);
+}
+
+
+function renderScene() {
+    renderer.render(scene, camera);
+}
